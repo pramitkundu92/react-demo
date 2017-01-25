4354fcc1adc1550b5784ecb90c985bb70c6e3a1f
US\prkundu <prkundu@deloitte.com>
2017-01-25 18:27:45 +0530

    changed CSS to LESS code and added LESS compilation using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,18 @@ module.exports = function(grunt){
                 }
             }
         },
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    optimization: 2,
+                    cleancss: true    
+                },
+                files: {
+                    'webapp/assets/styles.css': ['webapp/assets/styles.less']
+                }
+            }    
+        },
         concat: {
             options: {
                 separator: '/* Attaching new file */\n',
@@ -43,6 +55,14 @@ module.exports = function(grunt){
             }
         },
         watch: {
+            less: {
+                files: ['assets/styles.less'],
+                tasks: ['less','concat','cssmin'],
+                options: {
+                    interrupt: true,
+                    spawn: false
+                }
+            },
             js: {
                 files: ['webapp/app/*.js'],
                 tasks: ['concat'/*,'uglify'*/],
@@ -74,6 +94,7 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
     
-    grunt.registerTask('start',['concat',/*'uglify',*/'cssmin','express','watch']);
+    grunt.registerTask('start',['less','concat',/*'uglify',*/'cssmin','express','watch']);
 };
